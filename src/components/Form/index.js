f357013825857e1a85692b403a2a86273df7a750
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-12-12 11:07:47 -0300

    Added tweaks to Form CSS and HTML

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -18,30 +18,30 @@ class Form extends Component {
       <div className="job-form">
         <h1>Formulário para nova vaga</h1>
         <form>
-          <div class="form-group">
+          <div className="form-group">
             <label for="job-title">Título do Cargo</label>
-            <input type="text" class="form-control" id="job-title" placeholder="Exemplo: Auxiliar de Confeiteiro" />
+            <input type="text" className="form-control" id="job-title" placeholder="Exemplo: Auxiliar de Confeiteiro" />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="job-salary">Salário</label>
-            <input type="text" class="form-control" id="job-salary" placeholder="Exemplo: R$ 1800,00" />
+            <input type="text" className="form-control" id="job-salary" placeholder="Exemplo: R$ 1800,00" />
           </div>
-          <div class="form-group">
-            <label for="job-activities">Atividades do cargo</label>
-            <select multiple class="form-control" id="job-activities">
+          <div className="form-group">
+            <label for="job-activities">Atividades do cargo<span><br/>Após clicar no 1º item, segure CTRL para selecionar mais itens</span></label>
+            <select multiple className="form-control" id="job-activities">
               <option>Atendimento ao público</option>
               <option>Monitorar estoques de ingredientes de panificação</option>
-              <option>Preparar uma ampla variedade de produtos como bolos, cookies, tortas, pães, etc.</option>
-              <option>Criar sobremesas novas e interessantes para renovar nossos cardápios e atrair o interesse dos clientes</option>
-              <option>Decorar os doces usando diferentes glacês, coberturas, etc. para garantir uma apresentação bonita e estimulante</option>
-              <option>Verificar a qualidade do material e a condição dos equipamentos e dispositivos usados na cozinha</option>
+              <option>Preparar uma ampla variedade de produtos doces e salgados</option>
+              <option>Criar novas receitas de doces e salgados</option>
+              <option>Verificar a qualidade do material</option>
+              <option>Verificar a condição dos equipamentos e dispositivos usados na cozinha</option>
               <option>Orientar funcionários menos experientes</option>
               <option>Manter a organização da cozinha</option>
             </select>
           </div>
-          <div class="form-group">
-            <label for="job-benefits">Benefícios do Cargo</label>
-            <select multiple class="form-control" id="job-benefits">
+          <div className="form-group">
+            <label for="job-benefits">Benefícios do Cargo<span><br/>Após clicar no 1º item, segure CTRL para selecionar mais itens</span></label>
+            <select multiple className="form-control" id="job-benefits">
               <option>VA/VR</option>
               <option>Vale transporte</option>
               <option>Gympass</option>
@@ -52,9 +52,9 @@ class Form extends Component {
               <option>PJ</option>
             </select>
           </div>
-          <div class="form-group">
-            <label for="process-step">Etapas da vaga</label>
-            <select multiple class="form-control" id="process-step">
+          <div className="form-group">
+            <label for="process-step">Etapas da vaga<span><br/>Após clicar no 1º item, segure CTRL para selecionar mais itens</span></label>
+            <select multiple className="form-control" id="process-step">
               <option>Análise do currículo</option>
               <option>Entrevista com o RH</option>
               <option>Entrevista com o gestor</option>
@@ -62,9 +62,9 @@ class Form extends Component {
               <option>Contratação</option>
             </select>
           </div>
-          <div class="form-group">
-            <label for="job-skills">Habilidades necessárias</label>
-            <select multiple class="form-control" id="job-skills">
+          <div className="form-group">
+            <label for="job-skills">Habilidades necessárias<span><br/>Após clicar no 1º item, segure CTRL para selecionar mais itens</span></label>
+            <select multiple className="form-control" id="job-skills">
               <option>Criação de doces</option>
               <option>Criação de salgados</option>
               <option>Gestão de grupo</option>
@@ -72,15 +72,15 @@ class Form extends Component {
               <option>Administração</option>
             </select>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="job-experience">Experiência necessária</label>
-            <input type="text" class="form-control" id="job-experience" placeholder="Exemplo: 1 ano" />
+            <input type="text" className="form-control" id="job-experience" placeholder="Exemplo: 1 ano" />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="job-observations">Observações extras</label>
-            <textarea class="form-control" id="job-observations" rows="3"></textarea>
+            <textarea className="form-control" id="job-observations" rows="3"></textarea>
           </div>
-          <button type="submit" class="btn btn-info">Enviar</button>
+          <button type="submit" className="btn btn-info">Enviar</button>
         </form>
       </ div>
     )
