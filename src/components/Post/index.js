b790d3db1f761c97e754c0eaafb711facef44d03
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-12-12 10:17:43 -0300

    Added Post base

---
+++ src/components/Post/index.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+class Post extends Component{
+  state = {
+    jobTitle: '',
+    salary: '',
+    activities: [],
+    benefits: [],
+    processSteps: [],
+    skills: [],
+    experienceRequired: '',
+    jobSubmitted: false
+  }
+
+  render(){
+    return (
+      <>
+      Hi!
+      </>
+    )
+  }
+}
+
+export default Post
